daa93c28cd96b36280e2b8d64cddffce6f10d30c
Uliana Semyonova <i@ulianasemyonova.ru>
2023-06-01 21:19:51 +0500

    Create test_stack.py Create TestStack class > create test_push method

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,17 @@
-"""Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+import src.stack
+
+
+class TestStack(unittest.TestCase):
+
+    def test_push(self):
+        stack = src.stack.Stack()
+        stack.push('test_data')
+
+        self.assertEqual(stack.top.data, 'test_data')
+        self.assertIsInstance(stack.stack, list)
+        self.assertEqual(len(stack.stack), 1)
+
+        stack.push('test_data_2')
+
+        self.assertEqual(len(stack.stack), 2)
