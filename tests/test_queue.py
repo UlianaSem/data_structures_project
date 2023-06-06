0fd2015687e306ed083c5d376e21f25341533981
Uliana Semyonova <i@ulianasemyonova.ru>
2023-06-06 20:58:03 +0500

    Create test_queue.py

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,1 +1,26 @@
-"""Здесь надо написать тесты с использованием unittest для модуля queue."""
+import unittest
+import src.queue
+
+
+class TestQueue(unittest.TestCase):
+    def test_enqueue(self):
+        queue = src.queue.Queue()
+
+        queue.enqueue('data1')
+        queue.enqueue('data2')
+        queue.enqueue('data3')
+
+        self.assertEqual(queue.head.data, 'data1')
+        self.assertEqual(queue.head.next_node.data, 'data2')
+        self.assertEqual(queue.tail.data, 'data3')
+        self.assertIs(queue.tail.next_node, None)
+        with self.assertRaises(AttributeError):
+            queue.tail.next_node.data
+
+    def test_str(self):
+        queue = src.queue.Queue()
+
+        queue.enqueue('data1')
+        queue.enqueue('data2')
+
+        self.assertEqual(str(queue), "data1\ndata2")
